82defb65e3f30d993fa8c7758d30bd7f78065073
Mayank Jariwala <menickwa@gmail.com>
2019-06-26 01:06:15 -0400

    ## Swap Operation Functionality Provided

--- src/concordia/dems/communication/impl/EventManagerCommunicationOttawa.java
+++ src/concordia/dems/communication/impl/EventManagerCommunicationOttawa.java
@@ -15,6 +15,7 @@ import concordia.dems.communication.IEventManagerCommunicationPOA;
 import concordia.dems.helpers.Constants;
 import concordia.dems.helpers.EventOperation;
 import concordia.dems.helpers.Helper;
+import concordia.dems.model.enumeration.Servers;
 import concordia.dems.servers.OttawaUDPClient;
 
 /**
@@ -94,23 +95,20 @@ public class EventManagerCommunicationOttawa extends IEventManagerCommunicationP
 			return getBookingScheduleForClients(userRequest);
 		}
 
+		// Swap Event Functionality[Assignment 2 Functionality]
+		if (unWrappingRequest[Constants.ACTION_INDEX].equals(EventOperation.SWAP_EVENT)) {
+			return this.swapEventForCustomer(unWrappingRequest);
+		}
+
 		switch (unWrappingRequest[Constants.TO_INDEX]) {
 		case "montreal":
 			if (unWrappingRequest[Constants.ACTION_INDEX].equals(EventOperation.BOOK_EVENT)) {
-				unWrappingRequest[Constants.ACTION_INDEX] = EventOperation.GET_BOOKING_SCHEDULE;
-				boolean isNotEligible = isCustomerEligibleForBookingEvent(unWrappingRequest);
-				if (isNotEligible)
-					return "Limit Exceeded! You have been already registered for 3 events for a specific month";
-				return ottawaUDPClient.sendMessageToMontrealUDP(userRequest);
+				return this.sendBookEventMessageToMontreal(unWrappingRequest, userRequest);
 			} else
 				return ottawaUDPClient.sendMessageToMontrealUDP(userRequest);
 		case "toronto":
 			if (unWrappingRequest[Constants.ACTION_INDEX].equals(EventOperation.BOOK_EVENT)) {
-				unWrappingRequest[Constants.ACTION_INDEX] = EventOperation.GET_BOOKING_SCHEDULE;
-				boolean isNotEligible = isCustomerEligibleForBookingEvent(unWrappingRequest);
-				if (isNotEligible)
-					return "Limit Exceeded! You have been already registered for 3 events for a specific month";
-				return ottawaUDPClient.sendMessageToTorontoUDP(userRequest);
+				return this.sendBookEventMessageToToronto(unWrappingRequest, userRequest);
 			} else
 				return ottawaUDPClient.sendMessageToTorontoUDP(userRequest);
 		case "ottawa":
@@ -146,7 +144,130 @@ public class EventManagerCommunicationOttawa extends IEventManagerCommunicationP
 		return Helper.checkIfEqualMoreThanThree(torontoEvents, montrealEvents,
 				unWrappingRequest[Constants.INFORMATION_INDEX]);
 	}
-	
+
+	/**
+	 * This function help communication layer to pass message to toronto server
+	 * 
+	 * @param unWrappingRequest Unwrapping Request
+	 * @param userRequest       Request of User
+	 * @return String
+	 */
+	private String sendBookEventMessageToToronto(String[] unWrappingRequest, String userRequest) {
+		unWrappingRequest[Constants.ACTION_INDEX] = EventOperation.GET_BOOKING_SCHEDULE;
+		boolean isNotEligible = isCustomerEligibleForBookingEvent(unWrappingRequest);
+		if (isNotEligible)
+			return "Limit Exceeded! You have been already registered for 3 events for a specific month";
+		return ottawaUDPClient.sendMessageToTorontoUDP(userRequest);
+	}
+
+	/**
+	 * This function help communication layer to pass message to montreal server
+	 * 
+	 * @param unWrappingRequest Unwrapping Request
+	 * @param userRequest       Request of User
+	 * @return String
+	 */
+	private String sendBookEventMessageToMontreal(String[] unWrappingRequest, String userRequest) {
+		unWrappingRequest[Constants.ACTION_INDEX] = EventOperation.GET_BOOKING_SCHEDULE;
+		boolean isNotEligible = isCustomerEligibleForBookingEvent(unWrappingRequest);
+		if (isNotEligible)
+			return "Limit Exceeded! You have been already registered for 3 events for a specific month";
+		return ottawaUDPClient.sendMessageToMontrealUDP(userRequest);
+	}
+
+	/**
+	 * Swap Function initiator - Entire responsible to handle book and cancel event
+	 * 
+	 * @param unWrappingRequest: UnMarshalled Request
+	 * @return String Response of Swap Operation
+	 */
+	private String swapEventForCustomer(String[] unWrappingRequest) {
+		String[] swapEventdata = unWrappingRequest[Constants.INFORMATION_INDEX].split(",");
+		String customerId = swapEventdata[0];
+		String newEventId = swapEventdata[1];
+		String newEventType = swapEventdata[2];
+		String addResponse, cancelResponse, responseStatus;
+		// User Request for booking: montreal,montreal,Book
+		// Event,MTLC1234,MTLA120319,SEMINAR
+		addResponse = this.swapOperationBookEvent(unWrappingRequest, customerId, newEventId, newEventType);
+		responseStatus = addResponse.split("-")[0].trim();
+		if (responseStatus.equalsIgnoreCase("success")) {
+			String finalResponse = responseStatus;
+			// Remove User From Existing Event
+			String oldEventId = swapEventdata[3];
+			// swapEventdata[4] = Old Event Type are ignore during cancellation process
+			cancelResponse = this.swapOperationCancelEvent(unWrappingRequest, customerId, oldEventId);
+			responseStatus = cancelResponse.split("-")[0].trim();
+			if (responseStatus.equalsIgnoreCase("success")) {
+				return finalResponse + "||" + responseStatus;
+			} else if (responseStatus.equalsIgnoreCase("rejected")) {
+				cancelResponse = this.swapOperationCancelEvent(unWrappingRequest, newEventId, newEventType);
+				return "Swap Operation cannot be performed,because the cancellation process is rejected for event "
+						+ oldEventId + " and the new registered event is rollback by server " + cancelResponse;
+			}
+		} else if (responseStatus.equalsIgnoreCase("rejected")) {
+			return "Swap Operation cannot be performed, because the booking event rejected";
+		}
+		return "";
+	}
+
+	/**
+	 * Function only responsible to perform book event operation during swap
+	 * operation
+	 * 
+	 * @param unWrappingRequest
+	 * @param customerId
+	 * @param newEventId
+	 * @param newEventType
+	 * @return String Response from Server
+	 */
+	private String swapOperationBookEvent(String[] unWrappingRequest, String customerId, String newEventId,
+			String newEventType) {
+		Servers server = Helper.getServerFromId(newEventId);
+		String addResponse;
+		String addUserRequest;
+		if (server.equals(Servers.MONTREAL)) {
+			addUserRequest = "ottawa,montreal," + EventOperation.BOOK_EVENT + "," + customerId + "," + newEventId + ","
+					+ newEventType;
+			addResponse = this.sendBookEventMessageToMontreal(unWrappingRequest, addUserRequest);
+		} else if (server.equals(Servers.TORONTO)) {
+			addUserRequest = "ottawa,toronto," + EventOperation.BOOK_EVENT + "," + customerId + "," + newEventId + ","
+					+ newEventType;
+			addResponse = this.sendBookEventMessageToToronto(unWrappingRequest, addUserRequest);
+		} else {
+			addUserRequest = "ottawa,ottawa," + EventOperation.BOOK_EVENT + "," + customerId + "," + newEventId + ","
+					+ newEventType;
+			addResponse = ottawaUDPClient.sendMessageToOttawaUDP(addUserRequest);
+		}
+		return addResponse;
+	}
+
+	/**
+	 * Function only responsible to perform cancel event operation during swap
+	 * operation
+	 * 
+	 * @param unWrappingRequest
+	 * @param customerId
+	 * @param newEventId
+	 * @param newEventType
+	 * @return String Response from Server
+	 */
+	private String swapOperationCancelEvent(String[] unWrappingRequest, String customerId, String oldEventId) {
+		Servers server = Helper.getServerFromId(oldEventId);
+		String cancelUserRequest, cancelResponse;
+		if (server.equals(Servers.MONTREAL)) {
+			cancelUserRequest = "ottawa,montreal," + EventOperation.CANCEL_EVENT + "," + customerId + "," + oldEventId;
+			cancelResponse = ottawaUDPClient.sendMessageToMontrealUDP(cancelUserRequest);
+		} else if (server.equals(Servers.TORONTO)) {
+			cancelUserRequest = "ottawa,toronto," + EventOperation.CANCEL_EVENT + "," + customerId + "," + oldEventId;
+			cancelResponse = ottawaUDPClient.sendMessageToTorontoUDP(cancelUserRequest);
+		} else {
+			cancelUserRequest = "ottawa,ottawa," + EventOperation.CANCEL_EVENT + "," + customerId + "," + oldEventId;
+			cancelResponse = ottawaUDPClient.sendMessageToOttawaUDP(cancelUserRequest);
+		}
+		return cancelResponse;
+	}
+
 	@Override
 	public void shutdown() {
 		orb.shutdown(false);
